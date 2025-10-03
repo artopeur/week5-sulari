ff848756fced72adf39a0d020a4c9c262bf72537
artopeur <arto.peurasaari@gmail.com>
2025-10-03 14:24:28 +0300

    test cases for base values.

--- test_cases/TimeParserTest.cpp
+++ test_cases/TimeParserTest.cpp
@@ -11,6 +11,14 @@ TEST(TimeParserTest, TestCaseCorrectTime) {
     ASSERT_EQ(time_parse(time_test),0);
 
 }
+TEST(timeParserTest, TestCaseIncorrectTime) {
+    char time_test[] = "256161";
+    ASSERT_EQ(time_parse(time_test),TIME_VALUE_ERROR);
+}
+TEST(timeParserTest, TestCaseArrayError) {
+    char a = 'Y';
+    ASSERT_EQ(time_parse(&a), TIME_ARRAY_ERROR);
+}
 
 // https://google.github.io/googletest/reference/testing.html
 // https://google.github.io/googletest/reference/assertions.html
